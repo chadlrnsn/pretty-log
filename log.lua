d043b8f3150ed6b13de19666fd1a21f12f62b59e
chadlrnsn <akhmetshin.l89@gmail.com>
2023-12-11 00:34:50 +0500

    Rewrited for gmod

--- log.lua
+++ log.lua
@@ -7,84 +7,65 @@
 -- under the terms of the MIT license. See LICENSE for details.
 --
 
-local log = { _version = "0.1.0" }
+log = { _version = "0.1.0" }
 
 log.usecolor = true
-log.outfile = nil
 log.level = "trace"
 
-
 local modes = {
-  { name = "trace", color = "\27[34m", },
-  { name = "debug", color = "\27[36m", },
-  { name = "info",  color = "\27[32m", },
-  { name = "warn",  color = "\27[33m", },
-  { name = "error", color = "\27[31m", },
-  { name = "fatal", color = "\27[35m", },
+    { name = "trace", color = Color(20, 98, 200) },
+    { name = "debug", color = Color(105, 159, 230) },
+    { name = "info", color = Color(86, 200, 20) },
+    { name = "warn", color = Color(235, 180, 0) },
+    { name = "error", color = Color(255, 102, 0) },
+    { name = "fatal", color = Color(240, 63, 63) },
 }
-
 
 local levels = {}
 for i, v in ipairs(modes) do
-  levels[v.name] = i
+    levels[v.name] = i
 end
-
 
 local round = function(x, increment)
-  increment = increment or 1
-  x = x / increment
-  return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * increment
+    increment = increment or 1
+    x = x / increment
+    return (x > 0 and math.floor(x + 0.5) or math.ceil(x - 0.5)) * increment
 end
-
 
 local _tostring = tostring
 
 local tostring = function(...)
-  local t = {}
-  for i = 1, select('#', ...) do
-    local x = select(i, ...)
-    if type(x) == "number" then
-      x = round(x, .01)
+    local t = {}
+    for i = 1, select('#', ...) do
+        local x = select(i, ...)
+        if type(x) == "number" then
+            x = round(x, 0.01)
+        end
+        t[#t + 1] = _tostring(x)
     end
-    t[#t + 1] = _tostring(x)
-  end
-  return table.concat(t, " ")
+    return table.concat(t, " ")
 end
-
 
 for i, x in ipairs(modes) do
-  local nameupper = x.name:upper()
-  log[x.name] = function(...)
-    
-    -- Return early if we're below the log level
-    if i < levels[log.level] then
-      return
+    local nameupper = x.name:upper()
+    log[x.name] = function(...)
+        -- Return early if we're below the log level
+        if i < levels[log.level] then
+            return
+        end
+
+        local msg = tostring(...) .. "\n"
+        local info = debug.getinfo(2, "Sl")
+        local lineinfo = info.short_src .. ":" .. info.currentline
+
+        -- Output to console
+        MsgC(x.color, string.Interpolate("[{S2} {S3}]:{LINE}: ", {
+            S2 = nameupper,
+            S3 = os.date("%H:%M:%S"),
+            LINE = lineinfo
+        }), color_white, msg)
+
     end
-
-    local msg = tostring(...)
-    local info = debug.getinfo(2, "Sl")
-    local lineinfo = info.short_src .. ":" .. info.currentline
-
-    -- Output to console
-    print(string.format("%s[%-6s%s]%s %s: %s",
-                        log.usecolor and x.color or "",
-                        nameupper,
-                        os.date("%H:%M:%S"),
-                        log.usecolor and "\27[0m" or "",
-                        lineinfo,
-                        msg))
-
-    -- Output to log file
-    if log.outfile then
-      local fp = io.open(log.outfile, "a")
-      local str = string.format("[%-6s%s] %s: %s\n",
-                                nameupper, os.date(), lineinfo, msg)
-      fp:write(str)
-      fp:close()
-    end
-
-  end
 end
-
 
 return log
